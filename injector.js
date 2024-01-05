60c60489bbb5d36ad9ba5a6886e3f89957462032
lilaf <102885633+Lilafian@users.noreply.github.com>
2024-01-04 20:59:31 -0600

    Create injector.js

---
+++ injector.js
@@ -0,0 +1,14 @@
+// created by lilaf @ strawberry milk software for evinjector
+// script tag injector
+
+let Code = document.createElement("input");
+Code.addEventListener('keypress', (event)=> {
+	if (event.key === "Enter") {
+  	  let temp = document.createElement("script");
+  	  temp.innerHTML = Code.value;
+  	  document.body.appendChild(temp);
+  	  setTimeout(()=>{
+     	  temp.remove();
+  	  }, 30000);
+	}
+});
